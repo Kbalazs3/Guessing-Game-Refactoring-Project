a06a068687fcafcf4d9007a6eb6274473a2a7afc
Kbalazs3 <kbalazs93@gmail.com>
2021-08-12 00:02:53 +0200

    Refactoring of the code is done

--- guessing_game.py
+++ guessing_game.py
@@ -1,49 +1,59 @@
+# This code is a number guessing game. 
+
 import math
 import random
-a = []
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 99: "))
-    while a[i] != g:
-        if g < a[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 99: "))
-        elif g > a[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 99: "))
-        else:
-            break
+
+
+def create_random_nums_list():
+    rand_nums = []
+    for num in range(10):
+        rand_nums.append(random.randint(1, 99))
+    return rand_nums
+
+
+def guessing_phase(rand_nums):
+    for n in range(10):
+        guess = int(input("Enter an integer from 1 to 99: "))
+        while rand_nums[n] != guess:
+            if guess < rand_nums[n]:
+                print("guess is low")
+                guess = int(input("Enter an integer from 1 to 99: "))
+            elif guess > rand_nums[n]:
+                print("guess is high")
+                guess = int(input("Enter an integer from 1 to 99: "))
+            else:
+                break
     print("you guessed it!")
 
-b = []
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 49: "))
-    while b[i] != g:
-        if g < b[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 49: "))
-        elif g > b[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+
+def create_sec_rand_nums_list():
+    rand_nums_2 = []
+    for b in range(10):
+        rand_nums_2.append(random.randint(1, 49))
+    return rand_nums_2
+
+
+def sec_guessing_phase(rand_nums_2):
+    for i in range(10):
+        guess_2 = int(input("Enter an integer from 1 to 49: "))
+        while rand_nums_2[i] != guess_2:
+            if guess_2 < rand_nums_2[i]:
+                print("guess is low")
+                guess_2 = int(input("Enter an integer from 1 to 49: "))
+            elif guess_2 > rand_nums_2[i]:
+                print("guess is high")
+                guess_2 = int(input("Enter an integer from 1 to 49: "))
+            else:
+                break
+        print("you guessed it!")
+
+
+def main():
+    first_rand_num_list = create_random_nums_list()
+    guessing_phase(first_rand_num_list)
+    sec_rand_num_list = create_sec_rand_nums_list()
+    sec_guessing_phase(sec_rand_num_list)
+
+
+if __name__ == '__main__':
+    main()
